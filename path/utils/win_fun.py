a32bf1a01f88d87f9ed2da59ef0ce1469ec07194
jotahirubider <50107318+jotahirubider@users.noreply.github.com>
2021-10-22 09:14:38 +0200

    Refactoring

--- path/utils/win_fun.py
+++ path/utils/win_fun.py
@@ -1,21 +1,18 @@
 import os
 from subprocess import check_output
 
-
-
 class TaskKill:
     def firefox(self):
         """Kills from Windows CMD Firefox and Geckodriver Process"""
         if "firefox.exe" in str(check_output("tasklist")):
-            os.system('cmd /c "cd C:/ & taskkill /IM "firefox.exe" /F"')
+            a = os.system('cmd /c "cd C:/ & taskkill /IM "firefox.exe" /F"')
     def geckodriver(self):
         if "geckodriver.exe" in str(check_output("tasklist")):
-            os.system('cmd /c "cd C:/ & taskkill /IM "geckodriver.exe" /F"')
+            a = os.system('cmd /c "cd C:/ & taskkill /IM "geckodriver.exe" /F"')
     def ie(self):
         """Kills from Windows CMD IE and IEDriver Process"""
         if "iexplore.exe" in str(check_output("tasklist")):
-            os.system('cmd /c "cd C:/ & taskkill /IM "iexplore.exe" /F"')
+            a = os.system('cmd /c "cd C:/ & taskkill /IM "iexplore.exe" /F"')
     def iedriver(self):
         if "IEDriverServer.exe" in str(check_output("tasklist")):
-            os.system('cmd /c "cd C:/ & taskkill /IM "IEDriverServer.exe" /F"')
-
+            a = os.system('cmd /c "cd C:/ & taskkill /IM "IEDriverServer.exe" /F"')
